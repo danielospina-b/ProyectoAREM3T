5fca6aa9c3d13914ab71a3e011f758c3d9166e8a
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2018-05-19 17:49:00 -0500

    Correccion Errores. 	- Se soluciono error que no permitia ningun request al api a falta de un AuthenticationEntryPoint, se refactorizo el este.

--- Heroku/src/main/java/edu/eci/arem/facturacion/webapi/ApplicationConfig.java
+++ Heroku/src/main/java/edu/eci/arem/facturacion/webapi/ApplicationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 /**
  *
@@ -15,11 +16,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ApplicationConfig extends WebSecurityConfigurerAdapter {
     
+    private BasicAuthenticationEntryPoint baep;
+
+    public ApplicationConfig() {
+        this.baep = new BasicAuthenticationEntryPoint();
+        baep.setRealmName("Default");
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/*", "/css/*", "/js/*").permitAll()
         .anyRequest().authenticated();
+        http.httpBasic().authenticationEntryPoint(baep);
     }
     
     @Autowired
