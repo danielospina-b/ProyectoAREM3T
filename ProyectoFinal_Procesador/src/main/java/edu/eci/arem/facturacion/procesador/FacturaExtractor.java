75133833e12a686bc8d1b8ff0683af0cfb6fb358
Juan Camilo Mantilla <juan.mantilla-r@mail.escuelaing.edu.co>
2018-05-19 22:55:37 -0500

    Avance procesador de datos 	-Se extraen los datos del API y si hay nuevas facturas, se guardan 	en un archivo json en el directorio /procesados 	-Falta hacer DELETE de los recursos que se consultan en el API 	-Falta dejar el while funcionando

---
+++ ProyectoFinal_Procesador/src/main/java/edu/eci/arem/facturacion/procesador/FacturaExtractor.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arem.facturacion.procesador;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
+/**
+ *
+ * @author Juan Camilo Mantilla
+ */
+public class FacturaExtractor extends Thread {
+    @Override
+    public void run(){
+        //while(true){
+            try {
+                //Se revisa nuevasFacturas para determinar si hay nuevas por procesar
+                URL url = new URL ("http://localhost:8080/api/factura/nuevasFacturas");
+                String encoding = Base64.getEncoder().encodeToString("admin:1234".getBytes("UTF-8"));
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setDoOutput(true);
+                connection.setRequestProperty  ("Authorization", "Basic " + encoding);
+                InputStream content = (InputStream)connection.getInputStream();
+                BufferedReader in   = 
+                    new BufferedReader (new InputStreamReader (content));
+                String line;
+                String output=new String("");
+                while ((line = in.readLine()) != null) {
+                    //System.out.println(line);
+                    output=output+line;
+                }
+                
+                
+                //Si existen nuevas facturas, se pasan a archivos y se remueven del API
+                if(!output.equals("[]")){
+                    output = output.replace("[", "");
+                    output = output.replace("]", "");
+                    String[] facturasID = output.split(",");
+                    for(int i=0; i< facturasID.length; i++){
+                        URL url2 = new URL ("http://localhost:8080/api/factura/"+facturasID[i]);
+                        String encoding2 = Base64.getEncoder().encodeToString("admin:1234".getBytes("UTF-8"));
+
+                        HttpURLConnection connection2 = (HttpURLConnection) url2.openConnection();
+                        connection2.setRequestMethod("GET");
+                        connection2.setDoOutput(true);
+                        connection2.setRequestProperty  ("Authorization", "Basic " + encoding2);
+                        connection2.setRequestProperty("Content-Type", "application/json");
+                        InputStream content2 = (InputStream)connection2.getInputStream();
+                        BufferedReader in2   = 
+                            new BufferedReader (new InputStreamReader (content2));
+                        String line2;
+                        String output2=new String("");
+                        while ((line2 = in2.readLine()) != null) {
+                            //System.out.println(line2);
+                            output2=output2+line2;
+                        }
+                        PrintWriter writer = new PrintWriter("procesados/factura"+facturasID[i]+".json");                
+                        writer.println(output2);
+                        writer.close();
+                    }
+                }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+        //}
+    }
+}
